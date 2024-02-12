50c3efc9acbea96e8446556cdf2a8d6ae3779df6
Aaron Lal <lucidlear@gmail.com>
2024-02-12 18:51:11 +1100

    added restful API and fixed bugs, removed react integration and seperated out

--- api/views.py
+++ api/views.py
@@ -1,9 +1,18 @@
+from pickle import GET
+from turtle import update
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from . models import Note
 from . serializers import NoteSerializer
+from . utils import updateNote, getNoteDetail, deleteNote, getNotesList, createNote
+
+# /notes GET
+# /notes POST
+# /notes/<id> GET
+# /notes/<id> PUT
+# /notes/<id> DELETE
 
 # Create your views here.
 
@@ -45,46 +54,25 @@ def getRoutes(request):
     return Response(routes)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getNotes(request):
-  notes = Note.objects.all().order_by('-updated')
-  serializer = NoteSerializer(notes, many=True)
 
-  return Response(serializer.data)
+    if request.method == 'GET':
+        return getNotesList(request)
+
+    if request.method == 'POST':
+        return createNote(request)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def getNote(request, pk):
-  notes = Note.objects.get(id=pk)
-  serializer = NoteSerializer(notes, many=False)
 
-  return Response(serializer.data)
+    if request.method == 'GET':
+        return getNoteDetail(request, pk)
 
+    if request.method == 'PUT':
+        return updateNote(request, pk)
 
-@api_view(['POST'])
-def createNote(request):
-    data = request.data
-    note = Note.objects.create(body=data['body'])
-    serializer = NoteSerializer(note, many=False)
-    return Response(serializer.data)
-
-
-
-@api_view(['PUT'])
-def updateNote(request, pk):
-    data = request.data
-    note = Note.objects.get(id=pk)
-    serializer = NoteSerializer(instance=note, data=data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['DELETE'])
-def deleteNote(request, pk):
-    note = Note.objects.get(id=pk)
-    note.delete()
-    return Response('Note was deleted')
-
+    if request.method == 'DELETE':
+        return deleteNote(request, pk)
+        
--- mynotes/settings.py
+++ mynotes/settings.py
@@ -63,9 +63,7 @@ ROOT_URLCONF = 'mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'frontend/build'
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,10 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
